74e0ab51137bc0e14c695a54d65259f76ff35d90
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-08-20 09:53:53 -0700

    continued work on blog app

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import BlogPost, Comment
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Ok')
+    blogpost_list = BlogPost.objects.all()
+    context = {'blogposts':blogpost_list}
+    return render(request, 'blogapp/index.html', context)
