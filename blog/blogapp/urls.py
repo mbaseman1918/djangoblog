0fcd78de14fc8d3b16571196bd2018718d9417ac
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-08-20 13:51:13 -0700

    continued work on blog

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'blogapp' # for namespacing
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('add_blogpost/', views.add_blogpost, name='add_blogpost'),
+    path('delete_blogpost/', views.delete_blogpost, name='delete_blogpost'),
+    path('add_comment/', views.add_comment, name='add_comment'),
+    path('delete_comment/', views.delete_comment, name='delete_comment'),
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -7,5 +7,37 @@ from .models import BlogPost, Comment
 
 def index(request):
     blogpost_list = BlogPost.objects.all()
-    context = {'blogposts':blogpost_list}
+    comment_list = Comment.objects.all()
+    context = {
+    'blogposts':blogpost_list,
+    'comments':comment_list,
+    }
     return render(request, 'blogapp/index.html', context)
+
+def add_blogpost(request):
+    if request.method == "POST":
+        new_user = request.user.username
+        new_title = request.POST['title']
+        new_body = request.POST['body']
+        new_blogpost = BlogPost(user=new_user, title=new_title, body=new_body)
+        new_blogpost.save()
+    return HttpResponseRedirect(reverse('blogapp:index'))
+
+def delete_blogpost(request, pk):
+    target_blogpost = get_object_or_404(Blogpost, pk=pk)
+    target_blogpost.delete()
+    return HttpResponseRedirect(reverse('blogapp:index'))
+
+def add_comment(request, pk):
+    if request.method == "POST":
+        new_user = request.user.username
+        new_blogpost = BlogPost.objects.filter(pk=pk)[0]
+        new_body = request.POST['body']
+        new_comment = Comment(user=new_user, blogpost=new_blogpost, body=new_body)
+        new_comment.save()
+    return HttpResponseRedirect(reverse('blogapp:index'))
+
+def delete_comment(request, pk):
+    target_comment = get_object_or_404(Comment, pk=pk)
+    target_comment.delete()
+    return HttpResponseRedirect(reverse('blogapp:index'))
